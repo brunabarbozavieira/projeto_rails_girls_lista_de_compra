93a886815a4316b16492f82e99b5731ff0cd9af0
brunabarbozavieira <113641386+brunabarbozavieira@users.noreply.github.com>
2025-11-28 23:03:34 +0000

    Adiciona rota de criação de produto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new()
   end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path, notice: "Produto criado com sucesso!"
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def product_params
+  params.require(:product).permit(:name, :price, :quantity)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
 
   get    "products",          to: "products#index"
   get    "products/new",      to: "products#new"
+  post   "products",          to: "products#create"
 end
